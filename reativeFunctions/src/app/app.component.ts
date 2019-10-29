2553337ca5dac12f1258ede2e065a4d367eaff53
wcds <clark.kant@gmail.com>
2019-10-29 21:05:02 +0000

    Implementing example of forkJoin

--- reativeFunctions/src/app/app.component.ts
+++ reativeFunctions/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reativeFunctions';
+
+  ngOnInit(){
+
+    /*******************************/
+    //forkJoin example
+    let subjectA = new BehaviorSubject(2);
+    let subjectB = new BehaviorSubject("A");
+
+    forkJoin(subjectA,subjectB).subscribe((value)=>{
+       console.log("Both subjectA and subjectB has been executed");
+       value.forEach((element)=>{
+         console.log(element);
+       })
+    })
+
+    //It will be only executed when both subjectA and subjectB
+    //call complete()
+    subjectA.complete();
+    subjectB.complete();
+    /*******************************/
+    //forkJoin
+  }
 }
